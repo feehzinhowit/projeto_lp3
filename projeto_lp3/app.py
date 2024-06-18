933d16f2bfa8e9f8880fbf98df158c00dc9093f7
felipe witkowsky <felipewitkowsky1@gmail.com>
2024-06-18 11:28:19 -0300

    templates

--- projeto_lp3/app.py
+++ projeto_lp3/app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
+
+
 
 app = Flask("minha app")
 
@@ -8,15 +10,20 @@ app = Flask("minha app")
 # / - home page
 @app.route("/")
 def home ():
-    return "<h1>Home Page </h1>"
+    return render_template("home.html")
 
 # /contato - pagina de contato
 @app.route("/contato")
 def contato():
-    return "<h1>Contato</h1>"
+    return render_template("contato.html")
 
 
 # /produtos - pagina de produos 
 @app.route("/produtos")
 def produtos():
-    return "<h1>PRODUTOS</h1>"
+    lista_produtos = [
+        { "nome": "Coca-cola", "descricao": "mata a sede" },
+        { "nome": "Doritos", "descricao": "Suja a mão" },
+        { "nome": "Chocolate", "descricao": "Bom" }
+    ]
+    return render_template("produtos.html", produtos=lista_produtos)
