512002996de83fe8ae9d89a91a2a63815a2ab116
Felipe Witkowsky <felipewitkowsky1@gmail.com>
2024-06-25 11:22:53 -0300

    aula de forms, get, post

--- projeto_lp3/app.py
+++ projeto_lp3/app.py
@@ -1,12 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from validate_docbr import CPF, CNPJ
 
-
+lista_produtos = [
+        { "nome": "Coca-cola", "descricao": "mata a sede" },
+        { "nome": "Doritos", "descricao": "Suja a mão" },
+        { "nome": "Chocolate", "descricao": "Bom" }
+]
 
 app = Flask("minha app")
 
 
 # rota + função
+
+@app.route("/cpf")
+def cpf():
+    cpf = CPF()
+    GerarCpf = cpf.generate(True)
+    return render_template("cpf.html", cpf = GerarCpf)
+
+
+@app.route("/cnpj")
+def cnpj():
+    cnpj = CNPJ()
+    GerarCnpj = cnpj.generate(True)
+    return render_template("cnpj.html", cnpj = GerarCnpj)
 
 # / - home page
 @app.route("/")
@@ -22,30 +39,7 @@ def contato():
 # /produtos - pagina de produos 
 @app.route("/produtos")
 def produtos():
-    lista_produtos = [
-        { "nome": "Coca-cola", "descricao": "mata a sede" },
-        { "nome": "Doritos", "descricao": "Suja a mão" },
-        { "nome": "Chocolate", "descricao": "Bom" }
-    ]
     return render_template("produtos.html", produtos=lista_produtos)
-
-
-
-
-@app.route("/cpf")
-def cpf():
-    cpf = CPF()
-    GerarCpf = cpf.generate(True)
-    return render_template("cpf.html", cpf = GerarCpf)
-
-
-
-@app.route("/cnpj")
-def cnpj():
-    cnpj = CNPJ()
-    GerarCnpj = cnpj.generate(True)
-    return render_template("cnpj.html", cnpj = GerarCnpj)
-
 
 @app.route("/termosdeuso")
 def termosDeUso():
@@ -60,3 +54,30 @@ def politicasDePrivacidade():
 @app.route("/comoutilizar")
 def comoUtilizar():
     return render_template("comoUtilizar.html")
+
+# GET /produtos/cadastro devolver o form
+@app.route("/produtos/cadastro")
+def cadastro_produtos():
+    return render_template("cadastro_produtos.html")
+
+
+# POST /produtos que vai validar com os dados enviados pelo form
+# enviados pelo form
+#acessar o objeto request
+
+@app.route("/produtos", methods=['POST'])
+def salvar_produtos():
+
+    # pegando os valores digitados no form
+    # que estao na request
+    nome = request.form["nome"]
+    descricao = request.form["descricao"]
+
+    # crio um novo produto (novo dicionario)
+    produto = {"nome": nome, "descricao": descricao}
+
+    # adiciona o novo produto na lista 
+    lista_produtos.append(produto)
+
+    # devolvo o template com o novo produto
+    return render_template("produtos.html", produtos=lista_produtos)
